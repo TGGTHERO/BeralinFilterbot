3058eb3d8ab71ef0ac26c2ce32ded1f73fb02f54
ɢᴛ ᠰ ʜᴇʀᴏ <92996711+TGGTHERO@users.noreply.github.com>
2022-04-04 19:11:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -211,7 +211,8 @@ async def bot_status(client,message):
 
 @trojanz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    await message.reply_text(
+    await message.reply_photo(
+        photo=START_IMG
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
