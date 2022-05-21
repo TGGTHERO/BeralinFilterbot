bfdabde8d00650b11b3b7dd199a3b48e97e77596
ɢᴛ ᠰ ʜᴇʀᴏ <92996711+TGGTHERO@users.noreply.github.com>
2022-05-21 12:22:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -213,7 +213,7 @@ async def bot_status(client,message):
 async def start(client, message):
     await message.reply_photo(
         photo=START_IMG,
-        text=Script.START_MSG.format(message.from_user.mention),
+        caption=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
